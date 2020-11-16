a06c6a70734f4782044e755becefbc16fe052efb
Daniel Williams <daniel.williams@blip.pt>
2020-11-16 01:42:57 +0000

    fix addons version

--- packages/ui/components/Button/Button.stories.tsx
+++ packages/ui/components/Button/Button.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react-native';
 import {Button} from './Button';
-
+import {action} from '@storybook/addon-actions';
+import {text} from '@storybook/addon-knobs';
 storiesOf('button', module).add('mybutton', () => (
-  <Button onPress={() => null} text="hello world" />
+  <Button onPress={action('pressed')} text={text('button text', 'Hello')} />
 ));
